9c8ab6d43375a055f3a1289d088e55068e37b459
Babatunde Lamidi <babatundela@ha-shem.com>
2019-11-28 16:57:02 +0100

    chore: add asyncdata

--- pages/recipes/_id/index.vue
+++ pages/recipes/_id/index.vue
@@ -17,4 +17,38 @@
   text-align: center;
   padding: 30px;
 }
+
+.recipe-image {
+}
 </style>
+
+<script>
+export default {
+  // asyncData(context) {
+  //   return new Promise((resolve, reject) => {
+  //     setTimeout(() => {
+  //       resolve({
+  //         recipe: [
+  //           {
+  //             id: "1",
+  //             title: "Delicious",
+  //             previewText: "Awesome Meal",
+  //             descriptionText: "This is a really nice meal",
+  //             thumbnail:
+  //               "http://1.bp.blogspot.com/-98KPgn-NLk4/VN4YP3b1o0I/AAAAAAAACUM/Dvw6qUQeAuk/s1600/14251-Skewers%2BOn%2BA%2BPlate%2BFast%2BFood%2BHD%2BWallpaperz.jpg"
+  //           },
+  //           {
+  //             id: "2",
+  //             title: "Delicious Cuisine",
+  //             previewText: "Awesome Delicacy",
+  //             descriptionText: "Intercontinental Meal",
+  //             thumbnail:
+  //               "http://1.bp.blogspot.com/-98KPgn-NLk4/VN4YP3b1o0I/AAAAAAAACUM/Dvw6qUQeAuk/s1600/14251-Skewers%2BOn%2BA%2BPlate%2BFast%2BFood%2BHD%2BWallpaperz.jpg"
+  //           }
+  //         ]
+  //       });
+  //     }, 1500);
+  //   });
+  // }
+};
+</script>
--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,16 +1,12 @@
 <template>
   <section class="recipes">
     <Recipe
-      thumbnail="http://1.bp.blogspot.com/-98KPgn-NLk4/VN4YP3b1o0I/AAAAAAAACUM/Dvw6qUQeAuk/s1600/14251-Skewers%2BOn%2BA%2BPlate%2BFast%2BFood%2BHD%2BWallpaperz.jpg"
-      title="Heaveness"
-      id="1"
-      previewText="You know you want it"
-    ></Recipe>
-    <Recipe
-      thumbnail="https://i.pinimg.com/originals/7b/57/cb/7b57cbf75d820e2c968cc5b6bda2849d.jpg"
-      title="Delicious"
-      id="2"
-      previewText="Enjoy life inside life"
+      v-for="recipe in recipes"
+      :key="recipe.id"
+      :thumbnail="recipe.thumbnail"
+      :title="recipe.title"
+      :id="recipe.id"
+      :previewText="recipe.previewText"
     ></Recipe>
   </section>
 </template>
@@ -21,6 +17,33 @@ import Recipe from "@/components/Recipe";
 export default {
   components: {
     Recipe
+  },
+  asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: "1",
+              title: "Heaveness",
+              previewText: "You know you want it",
+              descriptionText: "This is a really nice meal",
+              thumbnail:
+                "http://1.bp.blogspot.com/-98KPgn-NLk4/VN4YP3b1o0I/AAAAAAAACUM/Dvw6qUQeAuk/s1600/14251-Skewers%2BOn%2BA%2BPlate%2BFast%2BFood%2BHD%2BWallpaperz.jpg"
+            },
+
+            {
+              id: "2",
+              title: "Delicious",
+              previewText: "Awesome Delicacy",
+              descriptionText: "Intercontinental Meal",
+              thumbnail:
+                "https://i.pinimg.com/originals/7b/57/cb/7b57cbf75d820e2c968cc5b6bda2849d.jpg"
+            }
+          ]
+        });
+      }, 1500);
+    });
   }
 };
 </script>
