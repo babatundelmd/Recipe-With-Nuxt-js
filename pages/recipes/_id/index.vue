80214622afbe90d797219c7ef6d7d0e9703d07f0
Babatunde Lamidi <babatundela@ha-shem.com>
2019-11-28 15:40:17 +0100

    chore: add dynamic path

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="single-recipe">
+    <h1>Title</h1>
+    <div>
+      <img src alt />
+    </div>
+    <p>The recipe desciption</p>
+  </section>
+</template>
+
+<style scoped>
+.single-recipe {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 30px;
+}
+</style>
--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -3,11 +3,13 @@
     <Recipe
       thumbnail="http://1.bp.blogspot.com/-98KPgn-NLk4/VN4YP3b1o0I/AAAAAAAACUM/Dvw6qUQeAuk/s1600/14251-Skewers%2BOn%2BA%2BPlate%2BFast%2BFood%2BHD%2BWallpaperz.jpg"
       title="Heaveness"
+      id="1"
       previewText="You know you want it"
     ></Recipe>
     <Recipe
       thumbnail="https://i.pinimg.com/originals/7b/57/cb/7b57cbf75d820e2c968cc5b6bda2849d.jpg"
       title="Delicious"
+      id="2"
       previewText="Enjoy life inside life"
     ></Recipe>
   </section>
@@ -25,6 +27,7 @@ export default {
 <style scoped>
 .recipes {
   display: flex;
+  flex-flow: column;
   flex-flow: row wrap;
   justify-content: center;
   align-items: center;
